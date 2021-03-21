4b88e35da0825734c992333ac94b5acee3a123e1
wongchan <1941256481@qq.com>
2021-03-21 15:39:53 +0800

    第一次提交

---
+++ wong-auth/src/main/java/com/wong/shopingmall/auth/WongAuthApplication.java
@@ -0,0 +1,17 @@
+package com.wong.shopingmall.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Wong
+ * @Description: 认证中心主启动类
+ * @DateTime: 2021/3/18 6:14 下午
+ **/
+@SpringBootApplication
+public class WongAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WongAuthApplication.class);
+    }
+}
